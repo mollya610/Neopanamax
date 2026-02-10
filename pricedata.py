cfe2dc7a1179138e7d13ade79334343087bd4d5c
mollya610 <malcor@unc.edu>
2026-02-09 20:55:48 -0500

    Create pricedata.py

---
+++ pricedata.py
@@ -0,0 +1,85 @@
+# getting price data from: 
+# https://pancanal.com/maritime-services/tarifas-maritimas/
+
+import pandas as pd 
+import numpy as np 
+
+
+#---- TRANSIT CHARGES --------- #  
+# REGULAR VESSEL 
+# Other 
+regular1 = 15000
+# Passengers, General Cargo and Refrigerated with CP/SUAB < 10,000
+regular2 = 25000
+# Container Ship; Vehicle Carrier/RoRo; Passenger Ship; Tanker; Chemical Tanker; LPG; LNG; Dry Bulk, Category “Other” with CP/SUAB ≥ 7,500; General Cargo and Refrigerated with CP/SUAB ≥ 10,000;
+regular3 = 60000
+###
+# SUPER VESSEL
+#Super Container Ship; Passenger Ship; Car Carrier/RoRo Ship; Tanker; LPG; LNG; Chemical Carrier; Dry Bulk Carrier; Refrigerated Carrier; General Cargo and “Other”
+super1 = 100000
+###
+# NEOPANAMAX VESSEL
+# Container Ship with TTA <10,000 TEU
+npnmx1 = 200000
+# Container ship with TTA ≥ 10,000 TEU
+# Tanker, Chemical tanker, LPG, LNG; Dry bulk; Passenger; Refrigerated, General cargo and “Other”
+npnmx2 = 300000 
+
+#---- CP SUAB CHARGES --------- #  
+# TANKERS 
+reg_tanker_cp = 6
+super_tanker_cp = 5.25
+npnmx_tanker_cp = 3.25 
+# CHEMICAL 
+reg_chemical_cp = 5.5
+super_chemical_cp = 5.25
+npnmx_chemical_cp = 3.25
+# VEHICLE 
+reg_vehicle_cp = 6.0
+super_vehicle_cp = 4.75
+npnmx_vehicle_cp = 2.75
+# GEN CARGO 
+reg_under10k_cargo_cp = 3.5
+reg_over10k_cargo_cp = 3.25
+super_cargo_cp = 3
+npnmx_cargo_cp = 1.5
+# REFRIGERATED
+reg_under10k_fridge_cp = 3.5
+reg_over10k_fridge_cp = 3.25
+super_fridge_cp = 3
+npnmx_fridge_cp = 1.5
+# OTHER 
+reg_under7500_other_cp = 3
+reg_over7500_other_cp = 3.25
+super_other_cp = 3
+npnmx_other_cp = 1.5 
+
+#---- TPM CHARGES --------- #
+# DRY BULK (measure by deadweight tonnage)
+reg_drybulk_tpm = 1.65
+super_drybulk_tpm = 1.5
+npmnx_drybulk_tpm = 0.8
+
+#---- TEU CHARGES --------- # 
+# CONTAINERS 
+reg_teu_capacity = 32
+reg_teu_loaded   = 38
+reg_teu_empty    = 7
+
+super_teu_capacity = 35
+super_teu_loaded   = 40
+super_teu_empty    = 8
+
+npnmx_teu_capacity = 42
+npnmx_teu_loaded   = 48
+npnmx_teu_empty    = 9
+
+#---- M3 CHARGES --------- # 
+# LPG 
+reg_lpg_m3 = 3.5
+super_lpg_m3 = 3.85
+npnmx_lpg_m3 = 2.75
+# LNG 
+reg_lng_m3 = 3.5
+super_lng_m3 = 3.85
+npnmx_lng_m3 = 2.05
